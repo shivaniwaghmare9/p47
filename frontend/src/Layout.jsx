3b520198fcac6312a7b7070afd86d8d5989f0456
shivani <shivaniw69@gmail.com>
2025-11-23 23:01:51 +0530

    layout

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,25 +1,4 @@
 
-// import {Outlet} from "react-router-dom" 
-// import Header from "./Components/Header";
-// import Topnav from "./Components/Topnav";
-// import Footer from "./Components/Footer";
-// const Layout=()=>{
-//     return(
-//         <>
-//         <div id="Head">
-//         <Header/>
-//         <Topnav/>
-//         </div>
-//         <div className="outlet">
-//           <Outlet/>
-//         </div>
-         
-//          <Footer/>
-//         </>
-//     )
-// }
-// export default Layout;
-
 import {Outlet} from "react-router-dom" 
 import Header from "./Components/Header";
 import Topnav from "./Components/Topnav";
