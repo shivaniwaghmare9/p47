a67ba0da70bf3579f64716e300b3919e00d1b64f
shivani <shivaniw69@gmail.com>
2025-12-17 21:38:23 +0530

    alout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,7 @@ const App=()=>{
           <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path="home" element={<Home/>}/>
-        
-          
-          </Route>
+        </Route>
          </Routes>
      </BrowserRouter>
     </>
