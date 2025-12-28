d43b86e416385a85275a2082ed69e39da5402589
shivani <shivaniw69@gmail.com>
2025-12-28 23:16:44 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,48 @@
 
+// // import { BrowserRouter, Route, Routes } from "react-router-dom";
+// // import Layout from "./Layout";
+// // import Home from "./Pages/Home";
+
+
+
+// // const App=()=>{
+// //   return(
+// //     <>
+// //      <BrowserRouter>
+// //          <Routes>
+// //           <Route path="/" element={<Layout/>}>
+// //           <Route index element={<Home/>}/>
+// //           <Route path="home" element={<Home/>}/>
+// //         </Route>
+// //          </Routes>
+// //      </BrowserRouter>
+// //     </>
+// //   )
+// // }
+// // export default App;
+
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./Pages/Home";
+
+
+
+// const App=()=>{
+//   return(
+//     <>
+//      <BrowserRouter>
+//          <Routes>
+//           <Route path="/" element={<Layout/>}>
+//           <Route index element={<Home/>}/>
+//           <Route path="home" element={<Home/>}/>
+//         </Route>
+//          </Routes>
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
+
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import Layout from "./Layout";
 // import Home from "./Pages/Home";
